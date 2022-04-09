5e2dabb325cd8eb39100ff1a6681b1c9a8f48291
Geonyeol Park <msmn1729@gmail.com>
2022-04-10 01:02:54 +0900

    채팅기능 구현

--- src/Client.java
+++ src/Client.java
@@ -4,7 +4,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1", 7777);
+        Socket socket = new Socket("192.168.0.2", 7777);
         System.out.println("[클라]: 소켓 획득");
 
         InputStream is = socket.getInputStream();
@@ -18,30 +18,26 @@ public class Client {
                 OutputStream os = socket.getOutputStream();
                 System.out.println("[클라]: 출력스트림 획득");
                 BufferedOutputStream bos = new BufferedOutputStream(os, 8192);
-                OutputStreamWriter osw = new OutputStreamWriter(bos, StandardCharsets.UTF_8);
-                BufferedWriter bw = new BufferedWriter(osw);
 
                 InputStream inputStream = System.in;
                 BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, 8192);
-                InputStreamReader inputStreamReader = new InputStreamReader(bufferedInputStream, StandardCharsets.UTF_8);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-                String outputMsg;
-                while ((outputMsg = bufferedReader.readLine()) != null) {
-                    bw.write(outputMsg);
-                    bw.flush();
-                    System.out.println("flush 완료");
+                byte[] buffer = new byte[3];
+                int bufferLen;
+                while ((bufferLen = bufferedInputStream.read(buffer)) != -1) {
+                    bos.write(buffer, 0, bufferLen);
+                    bos.flush();
                 }
-                bw.flush();
+                bos.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
-//        thread.start();
+        thread.start();
 
-        String inputMsg;
-        while ((inputMsg = br.readLine()) != null) {
-            System.out.println(inputMsg);
+        String msg;
+        while ((msg = br.readLine()) != null) {
+            System.out.println(msg);
         }
         System.out.println("[클라]: 출력완료");
     }

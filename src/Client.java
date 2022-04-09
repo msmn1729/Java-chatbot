ba5aa505dd97c7e1180c16f1544c7ed60ba9d2a4
Geonyeol Park <msmn1729@gmail.com>
2022-04-10 02:32:13 +0900

    client 관점에서 채팅기록 파일 저장

--- src/Client.java
+++ src/Client.java
@@ -13,22 +13,45 @@ public class Client {
         InputStreamReader isr = new InputStreamReader(bis, StandardCharsets.UTF_8);
         BufferedReader br = new BufferedReader(isr);
 
+        // 채팅 기록 저장
+        FileOutputStream fileOutputStream = new FileOutputStream("src/chatLog/client_log");
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream, 8192);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(bufferedOutputStream, StandardCharsets.UTF_8);
+
         Thread thread = new Thread(() -> {
             try {
+                InputStream inputStream = System.in;
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, 8192);
+                InputStreamReader inputStreamReader = new InputStreamReader(bufferedInputStream, StandardCharsets.UTF_8);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
                 OutputStream os = socket.getOutputStream();
                 System.out.println("[클라]: 출력스트림 획득");
                 BufferedOutputStream bos = new BufferedOutputStream(os, 8192);
+                OutputStreamWriter osw = new OutputStreamWriter(bos, StandardCharsets.UTF_8);
+                BufferedWriter bw = new BufferedWriter(osw);
 
-                InputStream inputStream = System.in;
-                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, 8192);
+                int len;
+                char[] cBuffer = new char[2];
 
-                byte[] buffer = new byte[3];
-                int bufferLen;
-                while ((bufferLen = bufferedInputStream.read(buffer)) != -1) {
-                    bos.write(buffer, 0, bufferLen);
-                    bos.flush();
+                StringBuilder sb = new StringBuilder("[클라이언트]: ");
+                while ((len = inputStreamReader.read(cBuffer)) != -1) {
+                    String outputMsg = new String(cBuffer, 0, len);
+                    osw.write(outputMsg);
+                    osw.flush();
+                    for (int i = 0; i < len; i++) {
+                        if (outputMsg.charAt(i) == '\n') {
+                            outputStreamWriter.write(sb.toString() + '\n');
+                            outputStreamWriter.flush();
+                            sb.setLength(0); // sb초기화
+                            sb.append("[클라이언트]: ");
+                        } else {
+                            sb.append(outputMsg.charAt(i));
+                        }
+                    }
                 }
-                bos.close();
+                osw.close();
+                outputStreamWriter.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -38,6 +61,8 @@ public class Client {
         String msg;
         while ((msg = br.readLine()) != null) {
             System.out.println(msg);
+            outputStreamWriter.write("[서버]: " + msg + "\n");
+            outputStreamWriter.flush();
         }
         System.out.println("[클라]: 출력완료");
     }

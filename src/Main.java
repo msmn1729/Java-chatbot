b33fded434da419dd65b8aed067a1eab92096215
Geonyeol Park <msmn1729@gmail.com>
2022-05-01 00:31:26 +0900

    feat: calc 명령어 사칙연산 구현

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,25 +8,61 @@ public class Main {
         InputStreamReader inputStreamReader = new InputStreamReader(bufferedInputStream, StandardCharsets.UTF_8);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-        String command = "/cal 5 + 4";
-//        while ((command = bufferedReader.readLine()) != null) {
-        if (command.charAt(0) != '/') {
-            System.out.println("명령어의 첫 글자는 '/'으로 시작해야 합니다.");
-//                continue;
-        }
+        String command;
+        while ((command = bufferedReader.readLine()) != null) {
+            if (command.charAt(0) != '/') {
+                System.out.println("warning: 명령어의 첫 글자는 '/'으로 시작해야 합니다.");
+                continue;
+            }
 
-        String[] commandTokenizer = command.split(" ");
-        if (commandTokenizer[0].equals("/cal")) {
-            if (commandTokenizer[2].equals("+")) {
-                sum(commandTokenizer[1], commandTokenizer[3]);
+            String[] commandTokenizer = command.split(" ");
+            if (commandTokenizer[0].equals("/cal")) {
+                String leftOperand = commandTokenizer[1];
+                String operator = commandTokenizer[2];
+                String rightOperand = commandTokenizer[3];
+                calculator(leftOperand, operator, rightOperand);
             }
         }
-//        }
     }
 
-    public static void sum(String leftOperand, String rightOperand) {
-        int leftNum = Integer.parseInt(leftOperand);
-        int rightNum = Integer.parseInt(rightOperand);
-        System.out.println(leftNum + rightNum);
+    public static void calculator(String leftOperand, String operator, String rightOperand) {
+        int leftNum, rightNum;
+
+        try {
+            leftNum = Integer.parseInt(leftOperand);
+        } catch (Exception exception) {
+            System.out.println("error: `" + leftOperand + "`는 올바르지 않은 포맷입니다.");
+            System.out.println("다음과 같이 포맷 양식을 지켜주세요.");
+            System.out.println("`숫자 데이터만 가능합니다.`");
+            return;
+        }
+
+        try {
+            rightNum = Integer.parseInt(rightOperand);
+        } catch (Exception exception) {
+            System.out.println("error: `" + rightOperand + "`는 올바르지 않은 포맷입니다.");
+            System.out.println("다음과 같이 포맷 양식을 지켜주세요.");
+            System.out.println("`숫자 데이터만 가능합니다.`");
+            return;
+        }
+
+        switch (operator) {
+            case "+":
+                System.out.println(leftNum + rightNum);
+                break;
+            case "-":
+                System.out.println(leftNum - rightNum);
+                break;
+            case "*":
+                System.out.println(leftNum * rightNum);
+                break;
+            case "/":
+                if (rightNum == 0) {
+                    System.out.println("error: 0으로 나눌 수 없습니다.");
+                    return;
+                }
+                System.out.println(leftNum / rightNum);
+                break;
+        }
     }
 }
